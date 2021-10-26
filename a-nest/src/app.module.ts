1eedfbcb2e2294d8b71f9a58fa4a568217d884fa
unn04012 <unn04012@naver.com>
2021-10-27 01:08:54 +0900

    dotenv, morgan 구현

---
+++ a-nest/src/app.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [ConfigModule.forRoot()], // 메서드는 설정을 넣어주기 위함
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
